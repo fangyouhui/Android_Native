1dcd519b7548f300a206e13b5617a821dd383ebc
jikang20080730@163.com <jikang20080730@163.com>
2020-11-03 10:45:48 +0800

    提交新的内容

---
+++ app/src/main/java/com/pai8/ke/activity/me/RegisterActivity.java
@@ -0,0 +1,1 @@
+package com.pai8.ke.activity.me;import androidx.appcompat.app.AppCompatActivity;import android.os.Bundle;import com.pai8.ke.R;public class RegisterActivity extends AppCompatActivity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_register);    }}
