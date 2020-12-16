a175e3252945da2237f72fc55185d5319a757618
gelaiye <1359022945@qq.com>
2020-12-17 02:19:54 +0800

    购物车数量修改以及添加异常报错界面

---
+++ app/src/main/java/com/pai8/ke/activity/takeaway/widget/MaxHeightRecyclerView.java
@@ -0,0 +1,46 @@
+package com.pai8.ke.activity.takeaway.widget;
+
+import com.pai8.ke.R;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * @author Created by zzf
+ * @time 2020/12/17 1:37
+ * Description：
+ */
+public class MaxHeightRecyclerView extends RecyclerView {
+    private int mMaxHeight;
+
+    public MaxHeightRecyclerView(Context context) {
+        super(context);
+    }
+
+    public MaxHeightRecyclerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize(context, attrs);
+    }
+
+    public MaxHeightRecyclerView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initialize(context, attrs);
+    }
+
+    private void initialize(Context context, AttributeSet attrs) {
+        TypedArray arr = context.obtainStyledAttributes(attrs, R.styleable.MaxHeightRecyclerView);
+        mMaxHeight = arr.getLayoutDimension(R.styleable.MaxHeightRecyclerView_maxHeight, mMaxHeight);
+        arr.recycle();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (mMaxHeight > 0) {
+            heightMeasureSpec = MeasureSpec.makeMeasureSpec(mMaxHeight, MeasureSpec.AT_MOST);
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
