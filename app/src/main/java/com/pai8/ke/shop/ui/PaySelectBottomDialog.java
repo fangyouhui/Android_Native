128b7f7265ebca21035b8861a3f231fde1c7ae1e
lihongshi <125296757@qq.com>
2021-03-30 18:23:03 +0800

    qq

--- app/src/main/java/com/pai8/ke/shop/ui/PaySelectBottomDialog.java
+++ app/src/main/java/com/pai8/ke/shop/ui/PaySelectBottomDialog.java
@@ -1,4 +1,8 @@
 package com.pai8.ke.shop.ui;
 
-public class PaySelectBottomDialog {
+import com.lhs.library.base.BaseBottomDialogFragment;
+import com.lhs.library.base.NoViewModel;
+import com.pai8.ke.databinding.DialogBottomPaySelectBinding;
+
+public class PaySelectBottomDialog extends BaseBottomDialogFragment<NoViewModel, DialogBottomPaySelectBinding> {
 }
